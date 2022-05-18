00a62d9d630c8a650bffea727b29c907a2124f44
Andre Rousselet <andre098@hotmail.com>
2022-05-18 19:44:58 -0300

    Req 4: implementa lógica da função thunk nas actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,39 @@
-// Coloque aqui suas actions
+// typeActions
 export const SET_USER_EMAIL = 'SET_USER_EMAIL';
+export const REQUEST_API = 'REQUEST_API';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
+export const HANDLE_API_ERROR = 'HANDLE_API_ERROR';
 
+// Coloque aqui suas actions
 export const userEmail = (email) => ({
   type: SET_USER_EMAIL,
   payload: email,
 });
+
+export const requestCurrenciesApi = () => ({
+  type: REQUEST_API,
+});
+
+export const getCurrencies = (data) => ({
+  type: GET_CURRENCIES,
+  payload: data,
+});
+
+export const handleCurrenciesApiError = (error) => ({
+  type: HANDLE_API_ERROR,
+  payload: error,
+});
+
+// thunk function
+export function fetchCurrencies() {
+  return async (dispatch) => {
+    dispatch(requestCurrenciesApi());
+    try {
+      const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const data = await response.json();
+      dispatch(getCurrencies(data));
+    } catch (error) {
+      dispatch(handleCurrenciesApiError(error));
+    }
+  };
+}
