70146522c535de541c3b230647d5c678253afca9
Andre Rousselet <andre098@hotmail.com>
2022-05-18 19:48:30 -0300

    Finaliza req 4: chama função thunk dentro do coomponentDidMount através do mapDispatchToProps

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { fetchCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { fetchCurrenciesFromApi } = this.props;
+    fetchCurrenciesFromApi();
+  }
+
   render() {
     return (
       <>
@@ -14,4 +22,12 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapDispatchToProps = (dispatch) => ({
+  fetchCurrenciesFromApi: () => dispatch(fetchCurrencies()),
+});
+
+Wallet.propTypes = {
+  fetchCurrenciesFromApi: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Wallet);
