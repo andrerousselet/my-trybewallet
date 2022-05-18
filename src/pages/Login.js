8cd962c0e3ca7ab5fa171690d35670515fcd695a
Andre Rousselet <andre098@hotmail.com>
2022-05-18 17:50:25 -0300

    Finaliza req 1: cria estados locais, funções handler e de validação e usa mapDispatchToProps

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.validateInputs());
+  }
+
+  handleSubmit = (event) => {
+    const { email } = this.state;
+    const { dispatchUserEmail, history } = this.props;
+    event.preventDefault();
+    dispatchUserEmail(email);
+    history.push('/carteira');
+  }
+
+  validateInputs = () => {
+    const { email, password } = this.state;
+
+    const PASSWORD_MIN_LENGTH = 6;
+    const errorCases = [
+      !email.length,
+      // fonte do Regex -> https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+      !email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i),
+      !password.length,
+      password.length < PASSWORD_MIN_LENGTH,
+    ];
+    const validInputs = errorCases.every((error) => error !== true);
+
+    this.setState({
+      isDisabled: !validInputs,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          name="email"
+          type="email"
+          value={ email }
+          data-testid="email-input"
+          placeholder="Insira aqui o seu e-mail"
+          onChange={ this.handleChange }
+        />
+        <input
+          name="password"
+          type="password"
+          value={ password }
+          data-testid="password-input"
+          placeholder="Insira aqui a sua senha"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          onClick={ this.handleSubmit }
+          disabled={ isDisabled }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchUserEmail: (email) => dispatch(userEmail(email)),
+});
+
+Login.propTypes = {
+  dispatchUserEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
