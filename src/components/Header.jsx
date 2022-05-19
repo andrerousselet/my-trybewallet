a589f630321abfdea94bd681ad298e86495f91e6
Andre Rousselet <andre098@hotmail.com>
2022-05-19 12:30:07 -0300

    Adiciona estilo ao Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './Header.css';
 
 class Header extends React.Component {
   render() {
     const { userEmail } = this.props;
     return (
-      <header>
-        <h1>TrybeWallet</h1>
-        <div
-          data-testid="email-field"
-        >
-          { userEmail }
-        </div>
-        <div
-          data-testid="header-currency-field"
-        >
-          BRL
-        </div>
-        <div
-          data-testid="total-field"
-        >
-          { 0 }
-        </div>
+      <header className="header-content">
+        <h1 className="header-main-title">TrybeWallet</h1>
+        <section className="header-right">
+          <div
+            // className="header-user-email"
+            data-testid="email-field"
+          >
+            {`Email: ${userEmail}`}
+          </div>
+          <div
+            // className="header-total-expenses"
+            data-testid="total-field"
+          >
+            {`Despesa Total: R$ ${0}`}
+          </div>
+          <div
+            // className="header-currency"
+            data-testid="header-currency-field"
+          >
+            BRL
+          </div>
+        </section>
       </header>
     );
   }
