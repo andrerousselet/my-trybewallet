9ffc8138d880c701545dba087e66087e3f6c2652
Andre Rousselet <andre098@hotmail.com>
2022-05-19 19:30:26 -0300

    Req 6: chama dispatchExpenses dentro de handleSubmit passando state comoo props

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,15 +2,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import { fetchCurrencies } from '../actions';
+import { fetchCurrencies, setExpenses } from '../actions';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      value: '',
+      currency: 'USD',
+      method: '',
+      tag: '',
+      description: '',
+    };
+  }
+
   componentDidMount() {
     const { fetchCurrenciesFromApi } = this.props;
     fetchCurrenciesFromApi();
   }
 
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value === 'Selecione' ? '' : value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    const { dispatchExpenses } = this.props;
+    event.preventDefault();
+    dispatchExpenses(this.state);
+    this.setState((prev) => ({
+      id: prev.id + 1,
+      value: '',
+      currency: 'USD',
+      method: '',
+      tag: '',
+      description: '',
+    }));
+  }
+
   render() {
+    const { value, currency, method,
+      tag, description } = this.state;
     const { currencies } = this.props;
     return (
       <>
@@ -21,12 +56,20 @@ class Wallet extends React.Component {
             <input
               id="value"
               data-testid="value-input"
+              name="value"
+              value={ value }
+              onChange={ this.handleChange }
             />
           </label>
           <label htmlFor="currencies">
             Moeda:
-            <select id="currencies">
-              {currencies.map((currency) => <option key={ currency }>{currency}</option>)}
+            <select
+              id="currencies"
+              name="currency"
+              value={ currency }
+              onChange={ this.handleChange }
+            >
+              {currencies.map((coin) => <option key={ coin }>{coin}</option>)}
             </select>
           </label>
           <label htmlFor="payment-method">
@@ -34,6 +77,9 @@ class Wallet extends React.Component {
             <select
               id="payment-method"
               data-testid="method-input"
+              name="method"
+              value={ method }
+              onChange={ this.handleChange }
             >
               <option>Selecione</option>
               <option>Dinheiro</option>
@@ -46,6 +92,9 @@ class Wallet extends React.Component {
             <select
               id="categories"
               data-testid="tag-input"
+              name="tag"
+              value={ tag }
+              onChange={ this.handleChange }
             >
               <option>Selecione</option>
               <option>Alimentação</option>
@@ -60,8 +109,17 @@ class Wallet extends React.Component {
             <input
               id="description"
               data-testid="description-input"
+              name="description"
+              value={ description }
+              onChange={ this.handleChange }
             />
           </label>
+          <button
+            type="submit"
+            onClick={ this.handleSubmit }
+          >
+            ADICIONAR DESPESA
+          </button>
         </form>
       </>
     );
@@ -74,11 +132,13 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCurrenciesFromApi: () => dispatch(fetchCurrencies()),
+  dispatchExpenses: (state) => dispatch(setExpenses(state)),
 });
 
 Wallet.propTypes = {
   currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
   fetchCurrenciesFromApi: PropTypes.func.isRequired,
+  dispatchExpenses: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
