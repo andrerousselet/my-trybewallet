dba1981280cdd37f007fd126cbb2b6f4f75adedd
Andre Rousselet <andre098@hotmail.com>
2022-05-19 14:23:57 -0300

    Finaliza req 5: usando map nas currencies disponiveis atraves do mapStateToProps

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -11,23 +11,74 @@ class Wallet extends React.Component {
   }
 
   render() {
+    const { currencies } = this.props;
     return (
       <>
         <Header />
-        <main>
-          <p>Wallet</p>
-        </main>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input
+              id="value"
+              data-testid="value-input"
+            />
+          </label>
+          <label htmlFor="currencies">
+            Moeda:
+            <select id="currencies">
+              {currencies.map((currency) => <option key={ currency }>{currency}</option>)}
+            </select>
+          </label>
+          <label htmlFor="payment-method">
+            Método de pagamento:
+            <select
+              id="payment-method"
+              data-testid="method-input"
+            >
+              <option>Selecione</option>
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="categories">
+            Categoria:
+            <select
+              id="categories"
+              data-testid="tag-input"
+            >
+              <option>Selecione</option>
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input
+              id="description"
+              data-testid="description-input"
+            />
+          </label>
+        </form>
       </>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCurrenciesFromApi: () => dispatch(fetchCurrencies()),
 });
 
 Wallet.propTypes = {
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
   fetchCurrenciesFromApi: PropTypes.func.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
