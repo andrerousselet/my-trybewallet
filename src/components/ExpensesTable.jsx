bfa0834ca3d25475afc919eda10febef887ea0c2
Andre Rousselet <andre098@hotmail.com>
2022-05-20 13:00:47 -0300

    Finaliza req 9: chama action deleteExpense através do mapDispatchToProps

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './ExpensesTable.css';
+import { deleteExpense } from '../actions';
 
 class ExpensesTable extends Component {
   render() {
-    const { expenses } = this.props;
+    const { expenses, handleDelete } = this.props;
     return (
       <table>
         <thead>
@@ -27,15 +28,28 @@ class ExpensesTable extends Component {
             const currencyName = expense.exchangeRates[expense.currency].name;
             return (
               <tr className="table-rows" key={ expense.id }>
-                <td>{expense.description}</td>
-                <td>{expense.tag}</td>
-                <td>{expense.method}</td>
-                <td>{Number(expense.value).toFixed(2)}</td>
-                <td>{currencyName}</td>
-                <td>{Number(usedExchangeRate).toFixed(2)}</td>
-                <td>{Number(expense.value * usedExchangeRate).toFixed(2)}</td>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ Number(expense.value).toFixed(2) }</td>
+                <td>{ currencyName }</td>
+                <td>{ Number(usedExchangeRate).toFixed(2) }</td>
+                <td>{ Number(expense.value * usedExchangeRate).toFixed(2) }</td>
                 <td>Real</td>
-                <td>editar / excluir</td>
+                <td>
+                  <button
+                    type="button"
+                  >
+                    EDITAR
+                  </button>
+                  <button
+                    type="button"
+                    data-testid="delete-btn"
+                    onClick={ () => handleDelete(expense.id) }
+                  >
+                    EXCLUIR
+                  </button>
+                </td>
               </tr>);
           })}
         </tbody>
@@ -48,8 +62,13 @@ const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  handleDelete: (id) => dispatch(deleteExpense(id)),
+});
+
 ExpensesTable.propTypes = {
   expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleDelete: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(ExpensesTable);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpensesTable);
